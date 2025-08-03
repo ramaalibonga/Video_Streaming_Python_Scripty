a8796a77a67d85e15164cf6f21a3b1d1efaa6204
ramaalicoder <61113149+ramaalibonga@users.noreply.github.com>
2025-08-03 08:24:19 -0700

    Update video_stream.py

--- video_stream.py
+++ video_stream.py
@@ -1,18 +1,18 @@
 import cv2
 
-#intantiation of VideoCapture object
+#initialization of  cv2 object
 vid  = cv2.VideoCapture(0)
 vid.set(3,200)
 vid.set(2,200)
 
-# video width && height
+# set video width & height
 video_width = int(vid.get(3))
 video_height = int(vid.get(4))
 
 
 video_size = (video_width,video_height)
 
-#write video to the mp4 format
+#write video in mp4 format
 
 save_video = cv2.VideoWriter('stream_01.mp4',cv2.VideoWriter_fourcc(*'MJPG'),10,video_size)
 
@@ -25,7 +25,7 @@ while(vid.isOpened()):
       #press q button to quit.
      if cv2.waitKey(1) & 0xFF == ord('q'):
           break
-  
+# save video
 save_video.release()
 vid.release()
 cv2.destroyAllWindows()
